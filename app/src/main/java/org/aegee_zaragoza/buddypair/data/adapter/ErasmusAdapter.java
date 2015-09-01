69ad44305484b8f863f40fe5371cc2a76bc84964
dbarelop <dbarelop@gmail.com>
2015-09-02 00:22:03 +0200

    Implemented Erasmus list

---
+++ app/src/main/java/org/aegee_zaragoza/buddypair/data/adapter/ErasmusAdapter.java
@@ -0,0 +1,57 @@
+package org.aegee_zaragoza.buddypair.data.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.aegee_zaragoza.buddypair.R;
+import org.aegee_zaragoza.buddypair.data.Erasmus;
+import org.aegee_zaragoza.buddypair.data.Peer;
+
+import java.util.List;
+
+/**
+ * Created by dbarelop on 1/9/15.
+ */
+public class ErasmusAdapter extends ArrayAdapter<Erasmus> {
+    private Activity activity;
+    private int layoutResourceId;
+
+    public ErasmusAdapter(Activity activity, int textViewResourceId, List<Erasmus> peerList) {
+        super(activity, textViewResourceId, peerList);
+        this.activity = activity;
+        layoutResourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Erasmus item = getItem(position);
+        View v = null;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(layoutResourceId, null);
+        } else {
+            v = convertView;
+        }
+        TextView name = (TextView) v.findViewById(R.id.activity_peers_list_item_name);
+        TextView faculty = (TextView) v.findViewById(R.id.activity_peer_list_item_faculty);
+        TextView studies = (TextView) v.findViewById(R.id.activity_peer_list_item_studies);
+        ImageView avatar = (ImageView) v.findViewById(R.id.activity_peer_list_item_image);
+
+        name.setText(item.getName() + " " + item.getSurname());
+        faculty.setText(item.getFaculty());
+        studies.setText(item.getStudies());
+        if (item.isMale()) {
+            avatar.setImageResource(R.drawable.male_avatar);
+        } else {
+            avatar.setImageResource(R.drawable.female_avatar);
+        }
+
+        return v;
+    }
+}
--- app/src/main/java/org/aegee_zaragoza/buddypair/database/DatabaseHelper.java
+++ app/src/main/java/org/aegee_zaragoza/buddypair/database/DatabaseHelper.java
@@ -2,11 +2,11 @@ package org.aegee_zaragoza.buddypair.database;
 
 import android.util.Log;
 
+import org.aegee_zaragoza.buddypair.data.Erasmus;
 import org.aegee_zaragoza.buddypair.data.Peer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -43,21 +43,43 @@ public class DatabaseHelper {
     }
 
     /* QUERIES */
-    private static final String QUERY_PEERS_LIST =
+    private static final String QUERY_PEER_LIST =
             "select STUDENT.*, PEER.id as peer_id, PEER.*, COUNTRY.country_name as country_name, STUDIES.name as studies_name, FACULTY.name as faculty_name " +
             "from STUDENT " +
             "inner join PEER on STUDENT.id = PEER.peer " +
             "inner join COUNTRY on STUDENT.nacionality = COUNTRY.country_code " +
             "inner join STUDIES on STUDENT.studies = STUDIES.id " +
             "inner join FACULTY on STUDENT.faculty = FACULTY.id";
+    private static final String QUERY_ERASMUS_LIST =
+            "select STUDENT.*, ERASMUS.id as erasmus_id, ERASMUS.*, COUNTRY.country_name as country_name, STUDIES.name as studies_name, FACULTY.name as faculty_name " +
+                    "from STUDENT " +
+                    "inner join ERASMUS on STUDENT.id = ERASMUS.erasmus " +
+                    "inner join COUNTRY on STUDENT.nacionality = COUNTRY.country_code " +
+                    "inner join STUDIES on STUDENT.studies = STUDIES.id " +
+                    "inner join FACULTY on STUDENT.faculty = FACULTY.id";
 
     public static List<Peer> getPeers() {
         try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(QUERY_PEERS_LIST)) {
+             ResultSet rs = stmt.executeQuery(QUERY_PEER_LIST)) {
             List<Peer> res = new ArrayList<>();
             while (rs.next()) {
                 Peer p = extractPeer(rs);
                 res.add(p);
+            }
+            return res;
+        } catch (SQLException e) {
+            Log.e(e.getMessage(), e.toString(), e);
+            return null;
+        }
+    }
+
+    public static List<Erasmus> getErasmus() {
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(QUERY_ERASMUS_LIST)) {
+            List<Erasmus> res = new ArrayList<>();
+            while (rs.next()) {
+                Erasmus e = extractErasmus(rs);
+                res.add(e);
             }
             return res;
         } catch (SQLException e) {
@@ -87,5 +109,31 @@ public class DatabaseHelper {
         String notes = rs.getString("notes");
         Peer p = new Peer(id, name, surname, gender, birthdate, nacionality, email, phone, studies, faculty, register_date, peer, gender_preference, erasmus_limit, notes);
         return p;
+    }
+
+    private static Erasmus extractErasmus(ResultSet rs) throws SQLException {
+        int id = rs.getInt("erasmus_id");
+        String name = rs.getString("name");
+        String surname = rs.getString("surname");
+        boolean gender = rs.getBoolean("gender");
+        Date birthdate = rs.getDate("birthdate");
+        String nacionality = rs.getString("country_name");
+        String email = rs.getString("email");
+        String phone = rs.getString("phone");
+        String studies = rs.getString("studies_name");
+        String faculty = rs.getString("faculty_name");
+        Date register_date = rs.getDate("register_date");
+        int erasmus = rs.getInt("erasmus");
+        Boolean gender_preference = rs.getBoolean("gender_preference");
+        if (rs.wasNull()) {
+            gender_preference = null;
+        }
+        Date arrival_date = rs.getDate("arrival_date");
+        if (rs.wasNull()) {
+            arrival_date = null;
+        }
+        String notes = rs.getString("notes");
+        Erasmus e = new Erasmus(id, name, surname, gender, birthdate, nacionality, email, phone, studies, faculty, register_date, erasmus, gender_preference, arrival_date, notes);
+        return e;
     }
 }
