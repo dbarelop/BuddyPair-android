651aa6f5c5f5807a81011460879eb661e500477e
dbarelop <dbarelop@gmail.com>
2015-09-02 15:37:45 +0200

    Replaced FAB icon with Design Support Library's one

---
+++ app/src/main/java/org/aegee_zaragoza/buddypair/behavior/ScrollingFABBehavior.java
@@ -0,0 +1,45 @@
+package org.aegee_zaragoza.buddypair.behavior;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+import org.aegee_zaragoza.buddypair.R;
+
+public class ScrollingFABBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    private int toolbarHeight;
+
+    public ScrollingFABBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.toolbarHeight = getToolbarHeight(context);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        if (dependency instanceof AppBarLayout) {
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
+            int fabBottomMargin = lp.bottomMargin;
+            int distanceToScroll = fab.getHeight() + fabBottomMargin;
+            float ratio = dependency.getY() / (float) toolbarHeight;
+            fab.setTranslationY(-distanceToScroll * ratio);
+        }
+        return true;
+    }
+
+    private static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+}
